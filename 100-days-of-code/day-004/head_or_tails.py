ffb02a46c928b7b1c4012b5236feb9eb0eff7325
RLMD <raphaelalmd@gmail.com>
2024-02-18 03:33:51 -0300

    Created Head or Tails program

---
+++ 100-days-of-code/day-004/head_or_tails.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+result = random.randint(0, 1)
+
+if result == 0:
+    print("Tails")
+else:
+    print("Head")
